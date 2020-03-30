3727a8beaecb7ce08e0a98612617ee01779b5dab
sarai-data-is-life <60305870+sarai-data-is-life@users.noreply.github.com>
2020-03-30 18:35:02 -0500

    Adding Additional Work

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "AIzaSyBVTeGtslbUZDDaJ7_MWuaVG8S7Dbd-QRk"
+weather_api_key = "a59e270b1bb83e8f312ff9b5fb2cb1e1"
 
 # Google API Key
 g_key = "AIzaSyBVTeGtslbUZDDaJ7_MWuaVG8S7Dbd-QRk"
